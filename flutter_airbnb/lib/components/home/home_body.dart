c10b20b61cab647ee8a1358cf976cd9a743db784
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-08-01 10:34:18 +0900

    에어비앤비 웹 화면 완성

--- flutter_airbnb/lib/components/home/home_body.dart
+++ flutter_airbnb/lib/components/home/home_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/components/home/home_body_banner.dart';
+import 'package:flutter_airbnb/components/home/home_body_popular.dart';
 import 'package:flutter_airbnb/size.dart';
 
 class HomeBody extends StatelessWidget {
@@ -7,18 +9,19 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double bodyWidth = getBodyWidth(context);
-    print("bodyWidth 70% : ${bodyWidth}");
+    // print("bodyWidth 70% : ${bodyWidth}");
 
-    return SizedBox(
-      width: bodyWidth,
-      child: Column(children: [
-        Container(
-          color: Colors.amberAccent,
-        ),
-        Container(
-          color: Colors.redAccent,
-        ),
-      ]),
+    return Align(
+      child: SizedBox(
+        width: bodyWidth,
+        child: Column(children: [
+          HomeBodyBanner(),
+          HomeBodyPopular(),
+          SizedBox(
+            height: gap_l,
+          )
+        ]),
+      ),
     );
   }
 }

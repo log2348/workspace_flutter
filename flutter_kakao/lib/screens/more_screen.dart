8c3bb88105fb2059293ee68fcba283d9be056ac3
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-08-04 09:22:59 +0900

    그리드뷰 활용한 더보기 화면 완성

--- flutter_kakao/lib/screens/more_screen.dart
+++ flutter_kakao/lib/screens/more_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
+import '../models/tap.dart';
+
 class MoreScreen extends StatelessWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("MoreScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          title: const Text(
+        '더보기',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 17,
+        ),
+      )),
+      body: Column(children: [
+        const SizedBox(
+          height: 30,
+        ),
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 4,
+            children: List.generate(
+              taps.length,
+              (index) => Column(
+                children: [
+                  Icon(taps[index].icon),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(taps[index].text)
+                ],
+              ),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }

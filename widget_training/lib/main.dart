ffd55224dbd54a196d0322f04066a32c20f47381
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-08-07 23:19:06 +0900

    TextField에 입력한 값 가져오기

--- widget_training/lib/main.dart
+++ widget_training/lib/main.dart
@@ -1,45 +1,62 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _textController = TextEditingController();
+  String result = '';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Spacer 연습'),
-            elevation: 0.0,
-            backgroundColor: Colors.green[200],
+        appBar: AppBar(
+          title: Text('TextField Test'),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(children: [
+          SizedBox(
+            height: 50,
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.redAccent,
-                ),
-                Spacer(),
-                CircleAvatar(
-                  backgroundColor: Colors.redAccent,
-                ),
-                Spacer(
-                  flex: 2,
-                ),
-                CircleAvatar(
-                  backgroundColor: Colors.blueAccent,
-                ),
-              ],
+          Text('아무거나 입력하세요'),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: TextField(
+              controller: _textController,
             ),
-          )),
+          ),
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  result = _textController.text;
+                  _textController.clear();
+                });
+              },
+              child: Text(
+                '보내기',
+                style: TextStyle(fontSize: 15),
+              )),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            result,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )
+        ]),
+      ),
     );
   }
 }
